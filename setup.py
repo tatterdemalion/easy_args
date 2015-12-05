5a089bbff7c5217e1e86924c91b2d12029e4d322
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2015-12-05 09:42:18 +0200

    download url added

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Can Mustafa Ozdemir',
     author_email='canmustafaozdemir@gmail.com',
     url='https://github.com/tatterdemalion/easy_args',
+    download_url='https://github.com/tatterdemalion/easy_args/tarball/0.1',
     packages=['easy_args'],
     test_suite='nose.collector',
     tests_require=['nose'],
