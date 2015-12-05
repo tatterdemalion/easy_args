6b7e4800fade85e640dc14ef5a11283ed93b4356
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2015-12-05 09:18:47 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Easy Args',
+    version='0.1',
+    description='Get system parameters easily',
+    author='Can Mustafa Ozdemir',
+    author_email='canmustafaozdemir@gmail.com',
+    url='https://github.com/tatterdemalion/easy_args',
+    packages=['easy_args'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
